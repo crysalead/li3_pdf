cdb57deb45a8c0c5133711a5f875a64b67304a7d
Christophe Vermeulen <christophe@cvermeul.fr>
2015-02-09 17:46:20 +0100

    Update Pdf.php

--- extensions/helper/Pdf.php
+++ extensions/helper/Pdf.php
@@ -11,9 +11,6 @@ class Pdf extends \lithium\template\Helper{
 		'svg' => 'li3_pdf\extensions\SVG_Document'
 	);
 	
-	/*
-	 * Holds the Svg instance
-	 */
 	public $_svg = null;
 	
 	public function _init($attrs = []){
@@ -35,34 +32,23 @@ class Pdf extends \lithium\template\Helper{
 
 	public function render() {
 		$doc = $this->_svg->render();
-//print_r($doc); exit;
-		// Extrait l'entête et le pied du Svg
 		$code = explode('<page', $doc);
 		$header = $code[0];
 		$code = explode('/page>', $doc);
 		$footer = $code[count($code) - 1];
-		// Extrait les différentes 'pages'
 		preg_match_all('/<page>(.*?)<\/page>/s', $doc, $body);
-//print_r(strlen($doc)); print_r($body); exit;
-		// Génére autant de fichier Svg qu'il y a de 'page' 
 		$files = [];
 		foreach ($body[1] as $index => $code) {
-			// Créé un fichier tmp
 			$tmp = tempnam(sys_get_temp_dir(), 'Svg');
-			// Ecrit l'entête du svg, le contenu de la 'page' et le pied du svg
 			file_put_contents($tmp, $header.$code.$footer);
 			$files[] = $tmp;
 		}
-
-		// Ligne de commande permettant de générer un PDF à partir de tous les fichiers Svg
 		$tmp = tempnam(sys_get_temp_dir(), 'Pdf');
 		$cmd = 'rsvg-convert -f pdf -o '.$tmp;
 		foreach ($files as $file) {
 			$cmd .= ' '.$file;
 		}
 		`$cmd`;
-//print_r($cmd); exit;
-		// Retourne le contenu du PDF
 		return file_get_contents($tmp);
 	}
 
